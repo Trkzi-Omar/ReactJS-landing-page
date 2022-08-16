920376024bd0a5c77acf72346623246e57cb2b91
TRKZI Omar <81875005+Trkzi-Omar@users.noreply.github.com>
2022-08-16 17:51:01 +0100

    Fixed the path to 'bootstrap/dist/css/bootstrap.css';

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Home from './components/Home';
 import {NavBar} from './components/NavBar';
-import 'bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import imgHorizontalLine from "./assets/img/line-1@1x.svg";
 
 function App() {
